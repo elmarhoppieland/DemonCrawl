aa08f29d70b1cc3e783dd20bdbece910261853b0
elmarhoppieland <114440420+elmarhoppieland@users.noreply.github.com>
2025-02-04 13:49:41 +0100

    Removed unnecessary Stats methods & properties

--- Resources/Scenes/Stats.gd
+++ Resources/Scenes/Stats.gd
@@ -1,136 +1,12 @@
 @tool
 extends MarginContainer
-class_name Stats
+class_name StatsDisplay
 
-# ==============================================================================
-#static var _instance: Stats :
-	#get:
-		#if not is_instance_valid(_instance):
-			#_instance = null
-		#return _instance
-
-#static var max_life: int = Eternal.create(0) :
-	#set(value):
-		#value = Effects.change_max_life(value)
-		#max_life = value
-		#if _instance:
-			#_instance._update_life_label()
-#static var life: int = Eternal.create(0) :
-	#set(value):
-		#life = value
-		#
-		#if _instance:
-			#_instance._update_life_label()
-#static var revives: int = Eternal.create(0) :
-	#set(value):
-		#value = Effects.change_revives(value)
-		#revives = value
-		#if _instance:
-			#_instance._update_revives()
-#static var defense: int = Eternal.create(0) :
-	#set(value):
-		#value = Effects.change_defense(value)
-		#defense = value
-		#if _instance:
-			#_instance._update_defense_label()
-#static var coins: int = Eternal.create(0) :
-	#set(value):
-		#value = Effects.change_coins(value)
-		#var increased := value > coins
-		#coins = value
-		#
-		#if not _instance:
-			#return
-		#
-		#_instance._update_coins_label()
-		#if increased:
-			#if not _instance._animation_player:
-				#await _instance.ready
-			#_instance._animation_player.play("coin_gain")
-#
-#static var untouchable: bool = Eternal.create(true)
-#
-#static var score: int = Eternal.create(0) :
-	#set(value):
-		#value = Effects.change_score(value)
-		#score = value
-#static var cells_opened_since_mistake: int = Eternal.create(0) :
-	#set(value):
-		#value = Effects.change_cells_opened_since_mistake(value)
-		#cells_opened_since_mistake = value
-#static var morality: int = Eternal.create(0) :
-	#set(value):
-		#value = Effects.change_morality(value)
-		#morality = value
-#static var chain: ChainData = Eternal.create(null) :
-	#set(value):
-		#value = Effects.change_chain(value)
-		#chain = value
-#static var chests_opened: int = Eternal.create(0) :
-	#set(value):
-		#value = Effects.change_chests_opened(value)
-		#chests_opened = value
-#static var monsters_killed: int = Eternal.create(0) :
-	#set(value):
-		#value = Effects.change_monsters_killed(value)
-		#monsters_killed = value
 # ==============================================================================
 @onready var _life_label: Label = %LifeLabel
 @onready var _defense_label: Label = %DefenseLabel
 @onready var _coins_label: Label = %CoinsLabel
-#@onready var _animation_player: AnimationPlayer = %AnimationPlayer
 # ==============================================================================
-
-## Deals [code]amount[/code] damage to the player. Before causing life loss,
-## propagates [code]damage(amount, source)[/code] through the [EffectManager].
-#static func damage(amount: int, source: Object = null) -> void:
-	#Stage.get_current().get_board().get_camera().shake()
-	#Stage.get_current().get_scene().get_background().flash_red()
-	#
-	#amount = Effects.damage(amount, source)
-	#if amount <= 0:
-		#return
-	#
-	#amount = maxi(1, amount - defense)
-	#
-	#change_life(-amount, source)
-	#untouchable = false
-
-
-## Changes the current [member life] by the given amount. Before changing the value,
-## propagates [code]change_life(amount, source)[/code] through the [EffectManager].
-## If the call changes the sign of [code]amount[/code], does not change [member life].
-#static func change_life(amount: int, source: Object = null) -> void:
-	#var original_sign := signi(amount)
-	#amount = Effects.change_life(amount, source)
-	#if signi(amount) != original_sign:
-		#return
-	#
-	#life = mini(life + amount, max_life)
-	#
-	#if life > 0:
-		#return
-	#
-	#Effects.death(source)
-	#if life > 0:
-		#return
-	#
-	#if revives > 0:
-		#revives -= 1
-		#revive()
-		#return
-	#
-	#Effects.player_lose(source)
-
-
-#static func set_life(value: int, source: Object = null) -> void:
-	#change_life(value - life, source)
-
-
-#static func spend_coins(amount: int, dest: Object = null) -> void:
-	#amount = Effects.spend_coins(amount, dest)
-	#coins -= amount
-
 
 func get_coin_position() -> Vector2:
 	return _coins_label.global_position
@@ -138,37 +14,6 @@ func get_coin_position() -> Vector2:
 
 func get_heart_position() -> Vector2:
 	return _life_label.global_position
-
-
-# TODO: should revive the player when called in the death() propagation without using any revives
-#static func revive(new_life: int = max_life) -> void:
-	#life = new_life
-
-
-#static func get_stats_tooltip_text() -> String:
-	#var parts := PackedStringArray([
-		#"%s: %d %s" % [TranslationServer.tr("SCORE"), score, TranslationServer.tr("POINTS_ABBR")],
-		#"%s: %d" % [TranslationServer.tr("CELLS_OPENED_SINCE_MISTAKE"), cells_opened_since_mistake],
-		#"%s: %d" % [TranslationServer.tr("MORALITY"), morality]
-	#])
-	#
-	#if chain and chain.length > 1:
-		#parts.append_array([
-			#"%s: %d %s" % [TranslationServer.tr("CHAIN_LENGTH"), chain.length, TranslationServer.tr("TURNS")],
-			#"%s: %d" % [TranslationServer.tr("CHAIN_VALUE"), chain.value],
-			#"%s: %d" % [TranslationServer.tr("CHAIN_SUM"), chain.get_sum()]
-		#])
-	#
-	#parts.append_array([
-		#"%s: %d" % [TranslationServer.tr("CHESTS_OPENED"), chests_opened],
-		#"%s: %d" % [TranslationServer.tr("MONSTERS_KILLED"), monsters_killed]
-	#])
-	#
-	#return "• " + "\n• ".join(parts)
-
-
-#func _init() -> void:
-	#_instance = self
 
 
 func _ready() -> void:
@@ -212,11 +57,3 @@ func _update_coins_label() -> void:
 		await ready
 	
 	_coins_label.text = str(Quest.get_current().get_stats().coins)
-
-
-#class ChainData:
-	#var length := 0
-	#var value := 0
-	#
-	#func get_sum() -> int:
-		#return length * value
--- Scenes/StageScene/Board/Objects/CellCoin.gd
+++ Scenes/StageScene/Board/Objects/CellCoin.gd
@@ -27,25 +27,6 @@ func _interact() -> void:
 	
 	tween_texture_to(get_stage().get_statbar().get_coin_position())
 	
-	#var texture := get_texture()
-	#var start_pos := get_cell().get_global_transform_with_canvas().origin + get_cell().size * get_cell().get_global_transform_with_canvas().get_scale() / 2
-	#var sprite := Stage.get_current().get_scene().tween_texture(texture, start_pos, Stats.get_coin_position(), 0.4, get_cell().get_sprite_material())
-	#var tween := sprite.create_tween().set_parallel()
-	#tween.tween_method(func(delta: float):
-		#texture.animate(get_animation_delta() * texture.get_tiles_area(), delta)
-	#, 0.0, 0.4, 0.4)
-	#tween.tween_property(sprite, "scale", Vector2.ZERO, 0.4).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_QUAD)
-	
-	#cell.global_sprite.texture = get_texture()
-	#var tween := cell.global_sprite.create_tween()
-	#tween.tween_property(cell.global_sprite, "position", Stats.get_coin_position() / 4, 0.4).from(cell.global_position)
-	#tween.parallel().tween_property(cell.global_sprite, "scale", Vector2.ZERO, 0.4)
-	#tween.parallel().tween_method(func(delta: float):
-		#cell.global_sprite.texture.animate(get_animation_delta(), delta)
-	#, 0.0, 0.4, 0.4)
-	#tween.tween_callback(cell.global_sprite.hide)
-	#cell.global_sprite.show()
-	
 	clear()
 
 
