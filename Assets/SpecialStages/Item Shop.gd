1654659a2595c9c3374a44a09df9dbb819fc5f8e
elmarhoppieland <114440420+elmarhoppieland@users.noreply.github.com>
2025-05-21 21:24:47 +0200

    Separated Engine & Assets
    Also fixed capitalization of Assets subdirectories

---
+++ Assets/SpecialStages/Item Shop.gd
@@ -0,0 +1,19 @@
+extends SpecialStage
+class_name ItemShop
+
+# ==============================================================================
+
+func _init() -> void:
+	type = Type.ITEM_SHOP
+	name = "Item Shop"
+	bg = preload("res://Assets/Backgrounds/item_shop.png")
+	icon = preload("res://Assets/sprites/special_icon/Item Shop.png")
+	icon_small = preload("res://Assets/sprites/special_icon/Item Shop_small.png")
+	
+	dest_scene = preload("res://Engine/Scenes/SpecialStages/Item Shop.tscn")
+
+
+static func _import(value: Dictionary) -> ItemShop:
+	var shop := ItemShop.new()
+	shop.locked = value.locked
+	return shop
