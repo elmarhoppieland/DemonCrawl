bce4f60be60607857341c7f95ecb53350e013b0c
elmarhoppieland <114440420+elmarhoppieland@users.noreply.github.com>
2025-08-10 17:28:51 +0200

    Added AuramancerUnlocker

--- Assets/Scripts/Masteries/AuramancerUnlocker.gd
+++ Assets/Scripts/Masteries/AuramancerUnlocker.gd
@@ -2,3 +2,28 @@ extends MasteryUnlocker
 class_name AuramancerUnlocker
 
 # ==============================================================================
+
+func _enter_tree() -> void:
+	get_quest().get_stage_effects().completed.connect(_stage_completed)
+
+
+func _exit_tree() -> void:
+	get_quest().get_stage_effects().completed.disconnect(_stage_completed)
+
+
+func _stage_completed() -> void:
+	var auras: Array[Script] = []
+	for cell in get_quest().get_current_stage().get_cells():
+		if cell.has_aura():
+			var aura: Script = cell.get_aura().get_script()
+			if aura not in auras:
+				auras.append(aura)
+				if auras.size() >= 10:
+					break
+	
+	if auras.size() >= 1:
+		unlock(1)
+	if auras.size() >= 5:
+		unlock(2)
+	if auras.size() >= 10:
+		unlock(3)
