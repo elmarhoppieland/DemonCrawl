b898376e1f85c8fe9d48db1b6ab0b00dfe354065
Plet53 <Plet53@thegame.com>
2025-11-12 12:49:15 -0700

    Landmark Texture Atlas

---
+++ assets/scripts/objects/landmark.gd
@@ -0,0 +1,9 @@
+@tool
+@abstract
+extends CellObject
+class_name Landmark
+
+# ==============================================================================
+
+func _interact() -> void:
+	pass
---
+++ assets/scripts/objects/landmarks/wishpool.gd
@@ -0,0 +1,8 @@
+@tool
+extends Landmark
+class_name Wishpool
+
+# ==============================================================================
+
+func _get_texture() -> Texture2D:
+	return Texture2D.new()
