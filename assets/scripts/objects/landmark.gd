418c9b10e8f936b61071f1daa91503e1cfe3c021
Plet53 <Plet53@thegame.com>
2025-11-12 17:58:24 -0700

    Wishpools start with 1 charge. Confirmed.

--- assets/scripts/objects/landmark.gd
+++ assets/scripts/objects/landmark.gd
@@ -7,3 +7,9 @@ class_name Landmark
 
 func _interact() -> void:
 	pass
+
+func get_quest() -> Quest:
+	var base := get_parent()
+	while base != null and base is not Quest:
+		base = base.get_parent()
+	return base
