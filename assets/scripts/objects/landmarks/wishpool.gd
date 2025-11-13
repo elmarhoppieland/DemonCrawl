7794c37d190c90da805585a031ead2cb2f53e324
Plet53 <Plet53@thegame.com>
2025-11-12 17:58:24 -0700

    Wishpools start with 1 charge. Confirmed.

--- assets/scripts/objects/landmarks/wishpool.gd
+++ assets/scripts/objects/landmarks/wishpool.gd
@@ -4,5 +4,27 @@ class_name Wishpool
 
 # ==============================================================================
 
+var reward
+var charges: int = 1
+var charge_cell_count: int = 0
+
+# ==============================================================================
+
 func _get_texture() -> Texture2D:
 	return Texture2D.new()
+
+
+func _can_interact() -> bool:
+	return true
+
+
+func get_stats():
+	pass
+
+
+class WishPoolEffects extends EventBus:
+	signal get_heal_amount(amount: int)
+	signal get_coin_value(value: int)
+	signal get_mana_value(value: int)
+	signal get_minion_count(value: int)
+	signal get_pathfinding_value(value: int)
