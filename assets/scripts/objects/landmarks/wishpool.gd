14b3de892ddfad1dfd8546198c834809d9b15e3c
Plet53 <Plet53@thegame.com>
2025-11-16 18:33:10 -0700

    wishpool tracks cells opened since last mistake, resets on mistake

--- assets/scripts/objects/landmarks/wishpool.gd
+++ assets/scripts/objects/landmarks/wishpool.gd
@@ -4,14 +4,16 @@ class_name Wishpool
 
 # ==============================================================================
 var reward: WishpoolReward
-var charges: int = 1
-var charge_cell_count: int = 0
+var charges: int
+var charge_cell_count: int
+var local_cells_since_last_mistake: int
 # ==============================================================================
 
 func _init():
 	# TODO: Research actual ranges of wishpool charge count
 	charge_cell_count = randi_range(20, 30)
-
+	charges = 1
+	local_cells_since_last_mistake = 0
 
 func _spawn():
 	var script: Script = null
@@ -22,5 +24,46 @@ func _spawn():
 	reward.notify_spawned()
 
 
+func _enter_tree() -> void:
+	if get_parent() is not CellData:
+		return
+	
+	get_quest().get_attributes().property_changed.connect(_attribute_changed)
+
+
+func _exit_tree() -> void:
+	if get_parent() is not CellData:
+		return
+	
+	get_quest().get_attributes().property_changed.disconnect(_attribute_changed)
+
+
+func _attribute_changed(attribute: StringName, value: Variant) -> void:
+	if attribute == &"cells_opened_since_last_mistake":
+		_cells_opened_since_last_mistake_changed(value)
+
+
+func _cells_opened_since_last_mistake_changed(cell_count: int) -> void:
+	if cell_count == 0:
+		local_cells_since_last_mistake = 0
+		return
+	
+	local_cells_since_last_mistake += cell_count - get_quest().get_attributes().cells_opened_since_mistake
+	
+	if local_cells_since_last_mistake >= charge_cell_count:
+		charges += local_cells_since_last_mistake / charge_cell_count
+		local_cells_since_last_mistake = local_cells_since_last_mistake % charge_cell_count
+
+
 func _can_interact() -> bool:
 	return true
+
+
+func _get_annotation_text() -> String:
+	var values = {
+		"total_reward": charges * reward.reward_per_charge,
+		"reward_per_charge": reward.reward_per_charge,
+		"charge_cell_count": charge_cell_count
+	}
+	
+	return (tr("landmark.wishpool." + reward.get_script_name().to_snake_case()) + " " + tr("landmark.wishpool.increase")).format(values)
