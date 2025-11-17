cc737cdb900004405fe121836545a9f56b579e3a
Plet53 <Plet53@thegame.com>
2025-11-16 18:33:10 -0700

    wishpool tracks cells opened since last mistake, resets on mistake

--- assets/scripts/objects/wishpool_rewards/wishpool_reward.gd
+++ assets/scripts/objects/wishpool_rewards/wishpool_reward.gd
@@ -21,22 +21,6 @@ func notify_spawned() -> void:
 	_spawn()
 
 
-## Virtual method to override the return value of [method get_description].
-func _get_description() -> String:
-	var values := {}
-	for prop in get_property_list():
-		if not prop.usage & PROPERTY_USAGE_SCRIPT_VARIABLE:
-			continue
-		values[prop.name] = tr(str(get(prop.name)))
-	
-	return tr("landmark.wishpool." + _get_script_name().to_snake_case().to_lower().replace("_", ".")).format(values)
-
-
-## Returns the Wishpool's description.
-func get_description() -> String:
-	return _get_description()
-
-
 @abstract func _perform() -> void
 
 
@@ -44,5 +28,5 @@ func perform() -> void:
 	_perform()
 
 
-func _get_script_name() -> String:
+func get_script_name() -> String:
 	return UserClassDB.script_get_class(get_script()).trim_prefix("Wishpool").trim_suffix("Reward")
