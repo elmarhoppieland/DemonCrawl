71bbe9d6396f05b9b4da646b76d54831cbe70943
Plet53 <Plet53@thegame.com>
2025-11-23 16:52:13 -0700

    Suds (Item will be helpful for Testing)

---
+++ assets/items/suds.gd
@@ -0,0 +1,25 @@
+@tool
+extends MagicItem
+
+# ==============================================================================
+const DIRECTIONS = [
+	Vector2i(0, -1),
+	Vector2i(1, 0),
+	Vector2i(0, 1),
+	Vector2i(-1, 0),
+]
+# ==============================================================================
+
+func _use() -> void:
+	for cell in await target_cell():
+		if cell.is_visible():
+			for dir in DIRECTIONS:
+				cell.send_projectile(BubbleProjectile, dir)
+
+
+func _post() -> void:
+	pass # Testing signal handling FIXME
+
+
+func _can_use() -> bool:
+	return super() and get_quest().has_current_stage()
