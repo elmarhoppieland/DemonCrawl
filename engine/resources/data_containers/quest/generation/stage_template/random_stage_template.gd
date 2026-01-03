affe3397bf87d9960a0e9cc540783e42b61b2bd9
elmarhoppieland <114440420+elmarhoppieland@users.noreply.github.com>
2026-01-03 12:31:35 +0100

    Fix RandomStageTemplate

--- engine/resources/data_containers/quest/generation/stage_template/random_stage_template.gd
+++ engine/resources/data_containers/quest/generation/stage_template/random_stage_template.gd
@@ -3,15 +3,10 @@ extends StageTemplate
 class_name RandomStageTemplate
 
 # ==============================================================================
-# TODO: filter this list (maybe add it to the register?)
-# (we don't want stages like the metagate or wester showing up here)
-static var stage_list := DirAccess.get_directories_at("res://assets/skins/")
-# ==============================================================================
 
 func _generate() -> Stage:
 	var stage := super()
-	var stage_name := stage_list[randi() % stage_list.size()]
-	stage.file = load("res://assets/skins/%s/%s" % [stage_name, stage_name])
+	stage.file = DemonCrawl.get_full_registry().stages.pick_random()
 	return stage
 
 
